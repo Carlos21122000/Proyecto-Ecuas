72565ac47c716a66660fec0f67c3d34237d73700
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-08-09 16:35:25 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -13,18 +13,29 @@ y0=3.0
 x=np.linspace(0,1,100)
 
 sol = integrate.odeint(f,y0,x)
+
 #print(sol)
 # grafica solucion numerica
-fig, axes = plt.subplots()
-plt.title("Solucion Numerica")
-plt.xlabel("Coordenada X")
-plt.ylabel("Coordenada Y")
-axes.plot(x,sol,'--')
+def graficarSolNumerica(x,sol):
+    fig, axes = plt.subplots()
+    plt.title("Solucion Numerica")
+    plt.xlabel("Coordenada X")
+    plt.ylabel("Coordenada Y")
+    axes.plot(x,sol,'--')
+    return plt.show()
+    
+
 
 # grafica solucion exacta
-fig, axes = plt.subplots()
-plt.title("Solucion Exacta")
-plt.xlabel("Coordenada X")
-plt.ylabel("Coordenada Y")
-axes.plot(x,exacta(x))
-plt.show()
+def graficarSolExacta(x):
+    fig, axes = plt.subplots()
+    plt.title("Solucion Exacta")
+    plt.xlabel("Coordenada X")
+    plt.ylabel("Coordenada Y")
+    axes.plot(x,exacta(x))
+    return plt.show()
+    
+    
+    
+graficarSolExacta(x)
+graficarSolNumerica(x,sol)
